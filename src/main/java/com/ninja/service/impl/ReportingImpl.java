a261247efaf061c245e9a05bf7ed57c7915bebb2
rakeshraheja89 <45666343+rakeshraheja89@users.noreply.github.com>
2018-12-12 22:20:54 +0530

    Update ReportingImpl.java

--- src/main/java/com/ninja/service/impl/ReportingImpl.java
+++ src/main/java/com/ninja/service/impl/ReportingImpl.java
@@ -1,5 +1,8 @@
 package com.ninja.service.impl;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,11 +14,45 @@ import com.ninja.service.ReportingSPI;
 @Service
 public class ReportingImpl implements ReportingSPI {
 
-	@Override
-	public List<ExchangeData> downloadData() {
+	private List<ExchangeData> readDataFromFile(String file) {
 		List<ExchangeData> exchangeDataList = new ArrayList<ExchangeData>();
-		// TODO Auto-generated method stub
+		try {
+			// Create an object of filereader
+			// class with CSV file as a parameter.
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line = br.readLine(); // Reading header, Ignoring while
+											// ((line = br.readLine()) != null
+											// && !line.isEmpty()) {
+			// we are going to read data line by line
+			while ((line = br.readLine()) != null && !line.isEmpty()) {
+				ExchangeData exchangeData = new ExchangeData();
+				try {
+					String[] fields = line.split(",");
+					exchangeData.setSource(fields[0]);
+					exchangeData.setStock(fields[1]);
+					exchangeData.setHighPrice(fields[2]);
+					exchangeData.setLowPrice(fields[3]);
+					exchangeData.setNoOfShare(fields[4]);
+					exchangeData.setCurrentPrice(fields[5]);
+					exchangeData.setCostOfPurchase(fields[6]);
+					exchangeData.setCurrentPortfolio(fields[7]);
+					exchangeData.setPercentageGain(fields[7]);
+				} catch (Exception e) {
+				}
+				exchangeDataList.add(exchangeData);
+			}
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		return exchangeDataList;
+	}
+
+	@Override
+	public List<ExchangeData> downloadData(String filePath) {
+		return readDataFromFile(filePath);
 	}
 
 }
